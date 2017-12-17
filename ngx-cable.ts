a762840ac8f741ea1741222e9554079cb7395750
Maxim Tyulenev <mr_makss@mail.ru>
2017-12-18 01:11:06 +0700

    ref

--- ngx-cable.ts
+++ ngx-cable.ts
@@ -7,23 +7,23 @@ export class NgXCable {
     constructor(
         private broadcaster: Broadcaster
     ) {
-        this.broadcaster = new Broadcaster();
     };
     public setCable = function(url) {
         this.cable = ActionCable.createConsumer(url);
     };
     public create = function(params) {
-        return this.cable.subscriptions.create(params, <ActionCable.CreateMixin>{
-            received: function (data) {
-                this.broadcaster.broadcast(params.channel, data);
+        let _this = this;
+        return this.cable.subscriptions.create(params, {
+            received: function (data: any) {
+                _this.broadcaster.broadcast(params.channel, data);
             }
         });
     };
     public send = function(data) {
-        this.subscription.send(data);
+        this.cable.subscriptions.subscriptions[0].send(data);
     };
     public perform = function(action, data) {
-        this.subscription.perform(action, data);
+        this.cable.subscriptions.subscriptions[0].perform(action, data);
     };
     public unsubscribe = function() {
         let _this = this;
