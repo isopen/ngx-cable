0bc701f4f085f5c49c12015a801f5607b51dad4b
Maxim Tyulenev <mr_makss@mail.ru>
2018-04-08 13:11:42 +0700

    native build, help, ref

---
+++ out_dir/native_build.help.md
@@ -0,0 +1,9 @@
+requires:<br>
+npm install ts-loader<br>
+npm install rxjs<br>
+npm install angular<br>
+npm install util<br>
+npm install @angular/core<br><br>
+
+requires a peer:<br>
+npm install zone.js@^0.8.4
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  devtool: "inline-source-map",
+  entry: "./index.ts",
+  output: {
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'out_dir'),
+    filename: 'ngxcable.js'
+  }
+};
