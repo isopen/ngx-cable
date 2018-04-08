c7be6afc20a620f73f79ac4deaa0d14cdeaec849
Maxim Tyulenev <mr_makss@mail.ru>
2018-04-08 13:18:50 +0700

    ref

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,6 @@ module.exports = {
   mode: "production",
   devtool: "inline-source-map",
   entry: "./index.ts",
-  output: {
-    filename: "bundle.js"
-  },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
   },
