c4ca87b8f566b4c00f6ed09306ae40bf36642e9c
ArielAleksandrus <ArielAleksandrus@users.noreply.github.com>
2018-06-10 22:35:35 -0300

    Update rxjs import
    On angular 6, we can import 'Subject' directly from 'rxjs'

--- broadcaster.ts
+++ broadcaster.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 
